9ead1264a1b3c3e25099d56050d755aafb6460ae
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-04-15 11:27:02 +0200

    feat(app,docs,web): csp header (#252)

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -9,8 +9,34 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   transpilePackages: ["ui", "icons", "shared"],
   output: "standalone",
