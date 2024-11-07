66f776a0a0d56976da93f347f8b8e54806ba3d3f
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-11-07 14:32:55 +0100

    fix(ui): text props

--- packages/ui/src/text/text.tsx
+++ packages/ui/src/text/text.tsx
@@ -3,6 +3,8 @@ import { forwardRef } from "react";
 import { BaseText, type TextProps } from "./base-text";
 import { TextWithTooltip } from "./text-with-tooltip";
 
+export type { TextProps } from "./base-text";
+
 export const Text = forwardRef<HTMLParagraphElement, TextProps>(function Text(props, ref) {
   if (!props.hideOverflow) return <BaseText ref={ref} {...props} />;
 
