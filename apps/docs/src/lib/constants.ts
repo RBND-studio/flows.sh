ff458545ec783a01cecabc1623d9df10874d8ee0
Vojtěch Vidra <vojtechvidra@gmail.com>
2025-02-09 10:56:36 +0100

    fix(docs): gh last update rate limit (#859)

--- apps/docs/src/lib/constants.ts
+++ apps/docs/src/lib/constants.ts
@@ -1,2 +1,4 @@
 export const PRODUCTION = process.env.NEXT_PUBLIC_ENV === "prod";
 export const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
+// eslint-disable-next-line turbo/no-undeclared-env-vars -- ignore
+export const GH_ACCESS_TOKEN = process.env.GH_ACCESS_TOKEN;
