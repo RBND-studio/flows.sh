258af4313dd86f9130dad7f59a849cd21fad0758
Ondřej Pešička <77627332+OPesicka@users.noreply.github.com>
2025-04-09 09:17:00 +0200

    feat(docs): update docs for optional userId (#1108)
    * feat(docs): update docs for optional userId
    
    * text

--- apps/docs/src/app/layout.config.tsx
+++ apps/docs/src/app/layout.config.tsx
@@ -1,8 +1,6 @@
 import { DocsLogo } from "components/docs-logo";
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
-import { Home16 } from "icons";
 import { links } from "lib/links";
-import { Icon, Logo } from "ui";
 
 /**
  * Shared layout configurations
@@ -15,17 +13,5 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: <DocsLogo />,
   },
-  links: [
-    {
-      text: "Open app",
-      url: links.logIn,
-      icon: <Logo type="pill" size={16} />,
-    },
-    {
-      text: "Website",
-      url: links.homePage,
-      icon: <Icon icon={Home16} />,
-    },
-  ],
   githubUrl: links.publicRepo,
 };
