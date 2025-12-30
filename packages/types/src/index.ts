cc0b2a22c961f564c51e878a6ec98cec29521b14
Vojtěch Vidra <23375601+VojtechVidra@users.noreply.github.com>
2025-12-30 19:54:01 +0100

    feat: add backend websocket app (#1692)
    * feat: add backend websocket app
    
    * feat: shared-backend package and move shared functions
    
    * fix: test issues
    
    * fix kafka messages
    
    * fix: backend projects dev
    
    * feat: add nginx local
    
    * chore: update turbo
    
    * fix: query
    
    * feat: deploy action
    
    * chore: update packages

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./action";
 export * from "./block";
 export * from "./kafka";
+export * from "./sdk";
 export * from "./state-memory";
+export * from "./websocket";
 export * from "./workflow";
