c264bcfff103faadda20d840e3c71192a341a771
Ondřej Pešička <77627332+OPesicka@users.noreply.github.com>
2026-01-17 11:27:11 +0100

    feat(docs): improve docs layout and split content into categories (#1918)
    * feat(docs): improve docs layout and split content into categories
    
    * fix 404
    
    * replace more images
    
    * feat: plausible not found, fix not found absolute position
    
    * improvements
    
    ---------
    
    Co-authored-by: Vojtěch Vidra <23375601+VojtechVidra@users.noreply.github.com>

--- apps/docs/index.d.ts
+++ apps/docs/index.d.ts
@@ -6,6 +6,12 @@ declare global {
       GH_ACCESS_TOKEN: string;
     }
   }
+  interface Window {
+    // Can be undefined if the Plausible script hasn't loaded or is blocked
+    plausible?: Plausible;
+  }
 }
+
+type Plausible = (eventName: string, context?: { props?: unknown; callback?: () => void }) => void;
 
 export {};
--- apps/docs/src/app/(docs)/layout.tsx
+++ apps/docs/src/app/(docs)/layout.tsx
@@ -1,11 +1,59 @@
 import { baseOptions } from "app/layout.config";
-import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { DocsLayout } from "fumadocs-ui/layouts/notebook";
+import { getPlatformUrls } from "lib/get-platform-urls";
+import { links } from "lib/links";
 import { source } from "lib/source";
 import type { ReactNode } from "react";
+import { Button } from "ui";
 
 export default function Layout({ children }: { children: ReactNode }): ReactNode {
+  // Additional sidebar tabs that use folders as root
+  // Platform is a catch-all for other pages
+  const sidebarTabs = [
+    {
+      title: "Blocks & Components",
+      url: "/blocks",
+    },
+    {
+      title: "SDK",
+      url: "/sdk",
+    },
+    {
+      title: "Guides",
+      url: "/guides",
+    },
+  ];
+
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
+    <DocsLayout
+      {...baseOptions}
+      tabMode="navbar"
+      nav={{ ...baseOptions.nav, mode: "top" }}
+      tree={source.pageTree}
+      links={[
+        { text: "Contact", url: "https://flows.sh/contact" },
+        {
+          children: (
+            <Button size="default" asChild>
+              <a href={links.signUp} target="_blank" rel="noopener">
+                Sign up
+              </a>
+            </Button>
+          ),
+          type: "custom",
+        },
+      ]}
+      sidebar={{
+        tabs: [
+          {
+            title: "Platform",
+            url: "/",
+            urls: getPlatformUrls({ source, sidebarTabs }),
+          },
+          ...sidebarTabs,
+        ],
+      }}
+    >
       {children}
     </DocsLayout>
   );
