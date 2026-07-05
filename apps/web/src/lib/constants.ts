b70a022ab057b3cca2e0e39092afd633ed11243a
Vojtěch Vidra <23375601+VojtechVidra@users.noreply.github.com>
2026-07-05 17:09:52 +0200

    fix: stage slack channel, homepage test (#2477)
    * fix: homepage test
    
    * fix: stage env

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const PRODUCTION = process.env.NEXT_PUBLIC_ENV === "prod";
+export const PRODUCTION = process.env.NEXT_PUBLIC_ENV === "production";
 export const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
